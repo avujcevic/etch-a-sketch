5dafe1b0f670819e7aca8b2d1dd8eccb28ac8dbe
andrew <a.vujcevic@gmail.com>
2022-10-06 18:53:49 -0600

    Modify grid fill to be 16x16 instead of 4x4

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const container = document.querySelector("#grid_container");
 
-for (let i = 1; i <= 16; i++) {
+for (let i = 1; i <= 256; i++) {
     const gridSquare = document.createElement("div");
-    gridSquare.style.cssText = "width: 150px; height: 150px; background-color: red; border-style: dashed; box-sizing: border-box;"
+    gridSquare.style.cssText = "width: 40px; height: 40px; background-color: red; border-style: dashed; box-sizing: border-box;"
     container.appendChild(gridSquare);
 }
