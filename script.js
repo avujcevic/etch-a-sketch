12bf428a5d9db5922d6d7c9d646a6fb9b330065c
andrew <a.vujcevic@gmail.com>
2022-10-06 18:30:44 -0600

    Add single square to container

---
+++ script.js
@@ -0,0 +1,6 @@
+const container = document.querySelector("#grid_container");
+const gridSquare = document.createElement("div");
+
+gridSquare.style.cssText = "width: 60px; height: 60px; background-color: red;"
+
+container.appendChild(gridSquare);
