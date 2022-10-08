f5cb0f8ccb6290cd9b82ec773c2306d1d98a6fd8
andrew <a.vujcevic@gmail.com>
2022-10-08 17:21:52 -0600

    Generate grid squares dynamically based on container sizse

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 const container = document.querySelector("#grid_container");
+const containerWidth = container.clientWidth;
 
-for (let i = 1; i <= 256; i++) {
+let gridSize = 64;
+
+for (let i = 1; i <= gridSize ** 2; i++) {
     const gridSquare = document.createElement("div");
-    gridSquare.style.cssText = "width: 40px; height: 40px;";
+    const gridSquareWidth = containerWidth / gridSize;
+
+    gridSquare.style.width = gridSquareWidth + "px";
+    gridSquare.style.height =  gridSquare.style.width;
+
     container.appendChild(gridSquare);
 
     gridSquare.addEventListener("mouseover", function (e) {
